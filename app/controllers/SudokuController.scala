a2f3646cf3fed55c7a643f8ebfcaba738eec119b
baek3004 <manggaraniv@gmail.com>
2019-10-13 09:48:22 +0200

    sbt run succeed, error classpath for WoodBlockPuzzle.jar

--- app/controllers/SudokuController.scala
+++ app/controllers/SudokuController.scala
@@ -3,20 +3,24 @@ package controllers
 import javax.inject._
 
 import play.api.mvc._
-import de.htwg.se.sudoku.Sudoku
-import de.htwg.se.sudoku.controller.controllerComponent.GameStatus
+//import de.htwg.se.sudoku.Sudoku
+//import de.htwg.se.sudoku.controller.controllerComponent.GameStatus
+//import de.htwg.se.woodblockpuzzle.controller.Controller
+//import de.htwg.se.woodblockpuzzle.WoodBlockPuzzle
 
 @Singleton
-class SudokuController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-  val gameController = Sudoku.controller
-  def sudokuAsText =  gameController.gridToString + GameStatus.message(gameController.gameStatus)
+class SudokuController @Inject()(cc: ControllerComponents) extends AbstractController(cc){
+//  val gameController = Sudoku.controller
+//  def sudokuAsText =  gameController.gridToString + GameStatus.message(gameController.gameStatus)
+//  val woodBlockPuzzle = new WoodBlockPuzzle
 
   def about= Action {
     Ok(views.html.index())
   }
 
   def sudoku = Action {
-    Ok(sudokuAsText)
+//    Ok(sudokuAsText)
+    Ok("Hello World")
   }
 
 }

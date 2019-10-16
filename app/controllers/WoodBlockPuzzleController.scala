da70fbf288588e2be1676d2f2be157bc116b3fd6
baek3004 <manggaraniv@gmail.com>
2019-10-16 10:59:09 +0200

    Connection to localhost successfull. implemented the more important routes game over browser now playable

--- app/controllers/WoodBlockPuzzleController.scala
+++ app/controllers/WoodBlockPuzzleController.scala
@@ -4,20 +4,45 @@ import de.htwg.se.woodblockpuzzle.controller.Controller
 import javax.inject._
 import play.api.mvc._
 import de.htwg.se.woodblockpuzzle.controller.Controller
-import de.htwg.se.woodblockpuzzle.aview.tui.Tui
+//import de.htwg.se.woodblockpuzzle.aview.tui.Tui
 import de.htwg.se.woodblockpuzzle.WoodBlockPuzzle
 
 @Singleton
 class WoodBlockPuzzleController @Inject() (cc: ControllerComponents) extends AbstractController(cc){
+  val gameController = WoodBlockPuzzle.controller
+//  var woodBlockPuzzleastext = ""
 
-  var woodBlockPuzzleastext = WoodBlockPuzzle.controller.showFieldWithCoordinates()
+  def getText(): String ={
+    var woodBlockPuzzleastext = "COUNT: "+ gameController.returnCount + "\t HIGHSCORE: "+ gameController.returnHighscore +
+      "\n" + gameController.showFieldWithCoordinates() + "\n" + gameController.showBlock(1) +
+      "\n" + gameController.showBlock(2) + "\n" + gameController.showBlock(3)+
+      "\n " + gameController.statusText
+    woodBlockPuzzleastext
+  }
+
   def about= Action {
     Ok(views.html.index())
   }
 
   def woodblock = Action {
-    Ok(woodBlockPuzzleastext)
+    Ok(getText)
   }
 
+  def reset = Action {
+    gameController.reset
+    Ok(getText)
+  }
+  def add(b:Int, x:Int, y:Int) = Action{
+    gameController.addBlock(b,x,y)
+    Ok(getText)
+  }
 
+  def reverse = Action {
+    gameController.reverse()
+    Ok(getText)
+  }
+  def giveup = Action {
+    gameController.giveup
+    Ok(getText)
+  }
 }
